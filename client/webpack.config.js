976d28b0c290c3c43c344c2c163f96f268092dde
AlvaroEMatos <alvaro1000@live.com>
2023-12-19 21:54:22 -0300

    add project build command

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,41 +6,56 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    filename: 'static/js/[name].bundle.js',
   },
   devServer:{
     static: {
       directory: path.join(__dirname, 'public'),
     },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      }
+    },
     hot: true,
-    port: 9000,
+    port: 3333,
   },
   module:{
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        loader: "babel-loader"
       },
       {
         test: /\.html$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "html-loader"
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/,
+        generator:{
+          filename: 'static/css/[name][ext]',
+        }
+      },
+      {
+        test: /\.(png|jpeg|jpg|webp|gif|ico|mp3|mp4)$/,
+        generator:{
+          filename: 'static/resources/[name][ext]',
         }
       },
     ],
   },
-  optimization : {
-    minimizer: [new CssMinimizerPlugin()],
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin()
+    ],
     realContentHash: false,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html',
     }),
   ]
 };
